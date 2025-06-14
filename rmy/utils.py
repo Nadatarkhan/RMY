d38cce862ad74eb7504e8b93334854c41915250d
Nadatarkhan <youremail@example.com>
2025-06-14 14:29:42 -0400

    update

--- rmy/utils.py
+++ rmy/utils.py
@@ -15,6 +15,17 @@ epw_columns = [
     'liquid_precipitation_depth', 'liquid_precipitation_quantity'
 ]
 
+def read_epw_file(epw_file_path):
+    return pd.read_csv(epw_file_path, header=None, skiprows=8, sep=',', names=[
+        'year', 'month', 'day', 'hour', 'minute', 'data_source_unct', 'temp_air', 'temp_dew', 'relative_humidity',
+        'atmospheric_pressure', 'etr', 'etrn', 'ghi_infrared', 'ghi', 'dni', 'dhi', 'global_hor_illum',
+        'direct_normal_illum', 'diffuse_horizontal_illum', 'zenith_luminance', 'wind_direction', 'wind_speed',
+        'total_sky_cover', 'opaque_sky_cover', 'visibility', 'ceiling_height', 'present_weather_observation',
+        'present_weather_codes', 'precipitable_water', 'aerosol_optical_depth', 'snow_depth',
+        'days_since_last_snowfall', 'albedo', 'liquid_precipitation_depth', 'liquid_precipitation_quantity'
+    ])
+
+
 def load_epw(path):
     return pd.read_csv(path, skiprows=8, header=None, names=epw_columns)
 
