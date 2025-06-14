5c1c5868a2f257b3c837c41560e473962afb924a
Nadatarkhan <youremail@example.com>
2025-06-14 12:58:13 -0400

    update

--- rmy/__init__.py
+++ rmy/__init__.py
@@ -1,1 +1,13 @@
+def read_epw_file(epw_file_path):
+    import pandas as pd
+    return pd.read_csv(epw_file_path, header=None, skiprows=8, sep=',', names=[
+        'year', 'month', 'day', 'hour', 'minute', 'data_source_unct', 'temp_air', 'temp_dew', 'relative_humidity',
+        'atmospheric_pressure', 'etr', 'etrn', 'ghi_infrared', 'ghi', 'dni', 'dhi', 'global_hor_illum',
+        'direct_normal_illum', 'diffuse_horizontal_illum', 'zenith_luminance', 'wind_direction', 'wind_speed',
+        'total_sky_cover', 'opaque_sky_cover', 'visibility', 'ceiling_height', 'present_weather_observation',
+        'present_weather_codes', 'precipitable_water', 'aerosol_optical_depth', 'snow_depth',
+        'days_since_last_snowfall', 'albedo', 'liquid_precipitation_depth', 'liquid_precipitation_quantity'
+    ])
+
+
 from .rmy_generation import run_full_rmy_pipeline
